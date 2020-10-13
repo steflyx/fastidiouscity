d2005d3e61f7b6fcf97ecdff673b62df8d826103
steflyx <stefano.agresti19@gmail.com>
2020-10-13 18:16:17 +0200

    Update load_weights.py

--- weights/load_weights.py
+++ weights/load_weights.py
@@ -8,10 +8,4 @@ BASE_URL = 'https://fastidiouscity-models.s3.eu-central-1.amazonaws.com/'
 for info in infos:
 	url = BASE_URL + info['link']
 	r = requests.get(url, allow_redirects=True)
-	open(info['link'], 'wb').write(r.content)
-
-url = 'https://fastidiouscity-models.s3.eu-central-1.amazonaws.com/agreement_predictor/tf_model.h5'
-
-r = requests.get(url, allow_redirects=True)
-
-open('agreement_predictor/tf_model.h5', 'wb').write(r.content)
+	open('../../local_server/weights/' + info['link'], 'wb').write(r.content)
