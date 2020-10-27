ba088760605be2d3de0b0e52ab5909a836d7d076
Stefano Agresti <stefano.agresti19@gmail.com>
2020-10-27 22:36:45 +0100

    Explanation front-end

---
+++ static/js/explanation.js
@@ -0,0 +1,33 @@
+/*
+
+Adds a button "Show why" that explains why a certain prediction was made
+Takes:
+ - text_to_explain
+ - predictor
+ - container: where to place the explanation
+Returns:
+ - HTML with explanation
+
+*/
+function add_show_why(container, predictor_name, text){
+	
+	var button = $(document.createElement('p')).text('Show why').css('color': 'blue');
+	button.attr('predictor', predictor_name);
+	button.attr('text_to_explain', text);
+	button.addClass('show_why');
+	$(container).append(button);
+	
+};
+
+$(document).on('click', '.show_why', function(){
+
+	$.getJSON($SCRIPT_ROOT + '/explain_prediction', {
+		'predictor_name': $(this).attr('predictor'),
+		'text': $(this).attr('text_to_explain')
+	}, function(data){
+
+		$(this).html(data.explanation);
+				
+	});
+
+};
